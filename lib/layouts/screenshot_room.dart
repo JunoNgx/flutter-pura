9e399f2ef8e65769ea2a811d6a71cbe9b6eb37b5
Juno Nguyen <juno.ngx@gmail.com>
2020-04-15 13:38:07 +0800

    Image is now saved to external storage in Pictures.

--- lib/layouts/screenshot_room.dart
+++ lib/layouts/screenshot_room.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'dart:typed_data';
 import 'package:flutter/rendering.dart';
@@ -43,16 +45,42 @@ class _ScreenshotRoomState extends State<ScreenshotRoom> {
       return _takeScreenshot();
     }
 
+    // Learning note: preparing image data
     ui.Image image = await boundary.toImage();
+
+    // Learning note: convert image to necessary byteData
     ByteData byteData = await image.toByteData(format: ui.ImageByteFormat.png);
-    Uint8List pngBytes = byteData.buffer.asUint8List();
+    // TODO figure out what this line does
+    Uint8List byteData2 = byteData.buffer.asUint8List();
 
-//    print(pngBytes);
+    //Learning note: preparing file name, including app name, color name, a timestamp, and the extension of PNG
+    String fileName = 'Pura ' + widget.color.name + ' ' + (DateFormat("yMd-Hms").format(DateTime.now()).toString() + '.png');
 
-    var filePath = await ImagePickerSaver.saveFile(
-        fileData: pngBytes,
-        title: "Pura/" + DateFormat("yMd-Hms-").format(DateTime.now()),
-    );
-    //TODO change path to pictures/Pura/
+    //Learning note: check if target folder exists
+//    const String pathToPictures = "/storage/emulated/0/Pictures/";
+//    const String appFolderName = "Pura";
+//    const String targetFolder = pathToPictures + appFolderName + "/";
+    const String targetFolder = "/storage/emulated/0/Pictures/Pura/";
+
+    //Learning note: recursive will create all necessary folders
+    if (!(await Directory(targetFolder).exists())) {
+      Directory(targetFolder).create(recursive: true);
+      debugPrint('Directory does not exist. Folder created.');
+    }
+
+    //Learning note: everything comes down to writing the file
+    final File file = File(targetFolder + fileName);
+    file.writeAsBytes(byteData2);
+    print('File saved: ' + targetFolder + fileName);
+  }
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/colorStorage.json');
   }
 }
